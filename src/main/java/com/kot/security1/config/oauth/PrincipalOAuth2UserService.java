138c71f3f15d7c2bfec94111df107c11c094f15b
kotddari <kotddari@gmail.com>
2023-07-07 11:34:38 +0900

    Update Facebook OAuth2 feature

--- src/main/java/com/kot/security1/config/oauth/PrincipalOAuth2UserService.java
+++ src/main/java/com/kot/security1/config/oauth/PrincipalOAuth2UserService.java
@@ -1,6 +1,9 @@
 package com.kot.security1.config.oauth;
 
 import com.kot.security1.config.auth.PrincipalDetails;
+import com.kot.security1.config.oauth.provider.FacebookUserInfo;
+import com.kot.security1.config.oauth.provider.GoogleUserInfo;
+import com.kot.security1.config.oauth.provider.OAuth2UserInfo;
 import com.kot.security1.model.User;
 import com.kot.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,8 @@ import org.springframework.stereotype.Service;
 public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
     @Autowired
     private UserRepository userRepository;
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     // Google로부터 받은 userRequest 데이터에 대한 후처리를 수행하는 함수
     // 함수 종료시 @AuthenticationPrincipal이 만들어진다.
@@ -27,16 +32,29 @@ public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
         // userRequest정보 -> loadUSer 함수 -> 구글로부터 회원 프로필 받음
         System.out.println("getAttributes: " + oAuth2User.getAttributes());
 
-        String provider = userRequest.getClientRegistration().getClientId(); // Google
-        String providerId = oAuth2User.getAttribute("sub");
+        // 회원가입 진행
+        OAuth2UserInfo oAuth2UserInfo = null;
+        // provider: google
+        if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            System.out.println("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        // provider: facebook
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+            System.out.println("페이스북 로그인 요청");
+            oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else {
+            System.out.println("우리는 구글과 페이스북만 지원해요...");
+        }
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
         String username = provider + "_" + providerId;
         String password = "getinthere";
-        String email = oAuth2User.getAttribute("email");
+        String email = oAuth2UserInfo.getEmail();
         String role = "ROLE_USER";
 
         User userEntity = userRepository.findByUsername(username);
         if (userEntity == null) {
-            System.out.println("구글 로그인이 최초입니다.");
+            System.out.println(provider + " 로그인이 최초입니다.");
             userEntity = User.builder()
                     .username(username)
                     .password(password)
@@ -47,7 +65,7 @@ public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
                     .build();
             userRepository.save(userEntity);
         } else {
-            System.out.println("구글 로그인을 이미 한적이 있습니다. 당신은 자동 회원 가입이 되어 있습니다.");
+            System.out.println(provider + "로그인을 이미 한적이 있습니다. 당신은 자동 회원 가입이 되어 있습니다.");
         }
         return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
     }
--- src/main/java/com/kot/security1/config/oauth/provider/OAuth2UserInfo.java
+++ src/main/java/com/kot/security1/config/oauth/provider/OAuth2UserInfo.java
@@ -3,5 +3,7 @@ package com.kot.security1.config.oauth.provider;
 public interface OAuth2UserInfo {
     String getProviderId();
     String getProvider();
+    String getEmail();
+    String getName();
 
 }
